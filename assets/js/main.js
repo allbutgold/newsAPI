4fa8092dfefaf7d6fbfc583f504fe2c5678af3f5
allbutgold <contact@leokieffer.com>
2023-02-17 10:39:09 +0100

    load content on page loading, replace articles with new search results

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,10 @@ const btn = document.querySelector('#searchBtn');
 const apiKey = 'd9ac8667cbf740f88d423a806e773628';
 const url = 'http://newsapi.org/v2/top-headlines?'
 
-
-btn.addEventListener('click', (element) => {
-    element.preventDefault();
-    const language = document.querySelector('#en').value; // default language value is 'de' for german
+function news() {
+    const language = document.querySelector('#en').value; // default language value is 'en' for english
     const world = document.querySelector('#input').value; // default input is world
-    const category = document.querySelector('#category').value; // default category is 'technology'
+    const category = document.querySelector('#category').value; // default category is 'general'
     // const country = document.querySelector('#country').value; 
     // console.log(world, language, country);
 
@@ -24,25 +22,15 @@ btn.addEventListener('click', (element) => {
         q: input,
         category: category
         // country: country
-
     });
 
 fetch(`${url}${params}`) 
     .then(response => response.json())
     .then(data => {
-        // console.log(data)
-        data.articles.forEach(dataPoint => {
-            // get the data from the api and save the values as variables
-            // const author = dataPoint.author;
-            const content = dataPoint.content;
-            const url = dataPoint.url;
-            const title = dataPoint.title;
-            const publishedAt = dataPoint.publishedAt;
-            const date = new Date(publishedAt).toDateString();
-            const urlToImage = dataPoint.urlToImage;
-            const source = dataPoint.source.name;
-            // console.log(author, content, description, url, title, publishedAt, urlToImage, source)
+        console.log(data)
 
+            document.querySelector('#newsContent').innerHTML = '';
+            data.articles.forEach(dataPoint => {
             // create HTML elements
             // container
             const articleContainer = document.createElement('article');
@@ -54,6 +42,17 @@ fetch(`${url}${params}`)
             const contentElement = document.createElement('p');
             const readMore = document.createElement('button');
             const dateElement = document.createElement('h4');
+
+            // get the data from the api and save the values as variables
+            // const author = dataPoint.author;
+            const content = dataPoint.content;
+            const url = dataPoint.url;
+            const title = dataPoint.title;
+            const publishedAt = dataPoint.publishedAt;
+            const date = new Date(publishedAt).toDateString();
+            const urlToImage = dataPoint.urlToImage;
+            const source = dataPoint.source.name;
+            // console.log(author, content, description, url, title, publishedAt, urlToImage, source)
 
             // write the values to the HTML elements
             imgElement.innerHTML = `<img src="${urlToImage}" >`;
@@ -75,12 +74,23 @@ fetch(`${url}${params}`)
             parentElement.appendChild(dateElement);
             parentElement.appendChild(readMore);
             // article container
+            // while (articleContainer.parentElement) {
+            //     parentElement.replaceChild(articleContainer.parentElement);
+            // }
+
             articleContainer.appendChild(parentElement);
             // append to the end of the body
-            document.body.appendChild(articleContainer);
+            document.querySelector('#newsContent').appendChild(articleContainer);
         })
     })
+}
+btn.addEventListener('click', (element) => {
+    element.preventDefault();
+    news();
 });
+
+news();
+
 
 
 
